c0d5cf5e029752576e4f204ec0eaee60fc69b86a
priyankapatel1234 <55329362+priyankapatel1234@users.noreply.github.com>
2020-07-08 17:47:03 +0530

    Diagonal difference

---
+++ daigonal.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    n = len(arr)
+    diag_pr = diag_sec = 0
+    for i in range(n):
+        diag_pr += arr[i][i]
+        diag_sec += arr[n-i-1][i]
+    
+    return abs(diag_pr - diag_sec)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
